b22db73baa5c9805c0cbde92b8599c8603f94822
Dipanshu Shukla <shuklascientist5@gmail.com>
2026-05-18 05:12:10 +0530

    init

---
+++ src/main/java/com/dipanshushukla/cop_map_core_service/CopMapCoreServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dipanshushukla.cop_map_core_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CopMapCoreServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CopMapCoreServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dipanshushukla/cop_map_core_service/CopMapCoreServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dipanshushukla.cop_map_core_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CopMapCoreServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
